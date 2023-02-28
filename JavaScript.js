99a5603116c085fd2a7306ef383b1a3543364c46
MatiasIvanSanchez <122896904+MatiasIvanSanchez@users.noreply.github.com>
2023-02-28 01:35:07 -0300

    Add files via upload

---
+++ JavaScript.js
@@ -0,0 +1,3 @@
+document.getElementById("boton").onclick = function(){
+    document.getElementById("Actividad 1 Camila").innerHTML = "Gestión de constancias de equipos de comunicación y tareas vinculadas a la pre y post producción de comunicación interna."
+}
